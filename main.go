bb46e35a3ab5fbf20c9d2b75207132519a8e09e9
jakl <james.ross.koval@gmail.com>
2025-10-28 02:10:28 -0700

    Use a map instead of an array for unique keys

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
+	"strings"
 	"time"
 
 	"github.com/go-vgo/robotgo"
@@ -13,64 +13,93 @@ import (
 )
 
 var isRunning bool = false
+var keysToPressMap = map[byte]bool{
+	'1': true,
+	'2': true,
+	'3': true,
+	'4': true,
+	'5': true,
+	'6': true,
+	'7': true,
+	'8': true,
+	'9': true,
+}
+var delayBetweenKeys = time.Millisecond * 50
+var delayBetweenCycles = time.Millisecond * 50
+
+func keysToPressString() string {
+	keys := make([]byte, 0, len(keysToPressMap))
+	for key := range keysToPressMap {
+		keys = append(keys, key)
+	}
+	return string(keys)
+}
 
 func main() {
-	fmt.Println("Starting Path of Exile macro engine...")
+	fmt.Println("Starting macro engine...")
 	go addEvents()
 
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	<-sigs // Wait for an interrupt signal to gracefully shut down
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Exiting application...")
+	for {
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if len(input) != 2 {
+			fmt.Println("Current keys are:", keysToPressString())
+			continue
+		}
+
+		var operation = input[0]
+		var keyboardKey = input[1]
+
+		switch operation {
+		case '+':
+			keysToPressMap[keyboardKey] = true
+			fmt.Printf("Added key %s to macro\n", string(keyboardKey))
+		case '-':
+			delete(keysToPressMap, keyboardKey)
+			fmt.Printf("Removed key %s from macro\n", string(keyboardKey))
+		default:
+			fmt.Println("Current keys are:", keysToPressString())
+		}
+	}
 }
 
-func press1() {
-	if !isRunning {
-		return
-	}
-	robotgo.KeyTap("1")
-	time.Sleep(time.Second * 7)
-	go press1()
-}
+func pressKeysAndCycle() {
+	for {
+		if !isRunning {
+			return
+		}
 
-func pressr() {
-	if !isRunning {
-		return
+		for key := range keysToPressMap {
+			robotgo.KeyTap(string(key))
+			time.Sleep(delayBetweenKeys)
+		}
+		time.Sleep(delayBetweenCycles)
 	}
-	robotgo.KeyTap("r")
-	time.Sleep(time.Millisecond * 3190)
-	go pressr()
-}
-
-func presse() {
-	if !isRunning {
-		return
-	}
-	robotgo.KeyTap("e")
-	time.Sleep(time.Millisecond * 3190)
-	go presse()
 }
 
 func startMacro() {
-	go press1()
-	time.Sleep(time.Millisecond * 50) // Delay to avoid using two actions at once
-	go pressr()
-	time.Sleep(time.Millisecond * 1595) // Delay to offset r and e skills, so they are alternating
-	go presse()
+	go pressKeysAndCycle()
+	// The above will press keys fairly robotically/rhythmically
+	// For specialized behavior, add new recursive functions below
 }
 
 func addEvents() {
-	fmt.Println("--- Please press alt + c to enable macro ---")
-	fmt.Println("---              alt + v to pause macro  ---")
-	hook.Register(hook.KeyDown, []string{"c", "alt"}, func(e hook.Event) {
+	fmt.Println("--- Please press alt + q to enable macro        ---")
+	fmt.Println("---              alt + w to pause macro         ---")
+	fmt.Println("--- Add keys with +<key> and remove with -<key> ---")
+	fmt.Println("--- List current keys by pressing enter         ---")
+	fmt.Println("Current keys are:", keysToPressString())
+	hook.Register(hook.KeyDown, []string{"q", "alt"}, func(e hook.Event) {
 		if !isRunning {
 			isRunning = true
 			go startMacro()
 		}
 	})
 
-	hook.Register(hook.KeyDown, []string{"v", "alt"}, func(e hook.Event) {
+	hook.Register(hook.KeyDown, []string{"w", "alt"}, func(e hook.Event) {
 		isRunning = false
 	})
 
